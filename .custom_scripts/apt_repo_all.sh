d468e45d6705ca24836e16586fea07a46d24156b
Ezekiel Baniaga <8464038+ezekielbaniaga@users.noreply.github.com>
2024-08-06 16:37:57 +0800

    custom scripts

---
+++ .custom_scripts/apt_repo_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# Contrib
+#     Packages in this area are freely licensed by the copyright holder but depend on other software that is not free. 
+# Non-Free
+#     Packages in this area have some onerous license condition restricting use or redistribution of the software. 
+# Non-Free-Firmware
+#     This section is restricted to device firmware. While packages in this area basically have the same underlying license restrictions as those in non-free, a special exception has been made for them: even if not being DFSG-free, packages from non-free-firmware are allowed to be included in the official installation images. 
+#
+# Read More: https://www.debian.org/distrib/packages
+#
+add-apt-repository -c contrib -c non-free -c non-free-firmware
+
---
+++ .custom_scripts/system_common_components.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# common resource monitoring
+apt install htop
+
+# better view of resource, only ~2mb in install size
+apt install btop
+
+# ls,ll alternative
+apt install exa
+
+# curl - client for url
+apt install curl
+
+# rg - search current dir for a regex pattern. commonly used for neovim
+apt install ripgrep
+
+# gcc, g++, configure, make -> developer toolchain and language compilers
+apt install build-essential
+
+# gitk - git log view 
+apt install gitk
+
+# fav sreensaver
+apt install cmatrix
