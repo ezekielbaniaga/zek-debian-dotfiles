3dad748c2d2c314d7fa556622a3c7a1949e861b6
Ezekiel Baniaga <8464038+ezekielbaniaga@users.noreply.github.com>
2024-10-24 05:02:46 +0800

    added some private use area

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,17 +4,24 @@ end
 
 function fish_greeting
     echo \n
-	echo ' ' ╭╼╸ ┏━━━━━━━━━━━━━━━━━━━━┓\tDebian GNU/Linux: (set_color cyan; uname -onrm; set_color normal)
-	echo ''╺╾┼╼╸ ┃ --= boy_deploy =-- ┃\tFish Shell: (set_color cyan; fish --version; set_color normal)
-	echo ' ' ╰╼╸ ┗━━━━━━━━━━━━━━━━━━━━┛\tDate: (set_color cyan; date; set_color normal)
+	echo ' ' ╭╼╸ ┏━━━━━━━━━━━━━━━━━━━━┓\t\uf306 Debian GNU/Linux: (set_color cyan; uname -onrm; set_color normal)
+	echo ''╺╾┼╼╸ ┃ --= boy_deploy =-- ┃\t\uf489 Fish Shell: (set_color cyan; fish --version; set_color normal)
+	echo ' ' ╰╼╸ ┗━━━━━━━━━━━━━━━━━━━━┛\t\ueab0 Date: (set_color cyan; date; set_color normal)
+	echo '                            '\t\ue38b Uptime: (set_color cyan; uptime --pretty; set_color normal)
+	echo '                            '\t\uf4e3 Since: (set_color cyan; uptime --since; set_color normal)
+	echo '                            '\t\uf004 Favs: (set_color cyan) \uf31a \ue61e \ue6a9 \ue62b \ue6ae \ue76e \ue702  (set_color normal)
     echo \n
 end
 
 function fish_prompt
-    printf '\n╺╾┬╼╸ %s %s@%s %s %s %s %s %s %s%s%s \n  │\n  ╰╼╸ ❱❯❭ ' \
-      (set_color -b cyan)(set_color black) $USER $hostname (set_color normal) \
-      (set_color -b brblack)(set_color black) (prompt_pwd) (set_color normal) \
-      (set_color cyan) (fish_git_prompt) (set_color normal)
+    printf '\n╺╾┬╼╸%s%s %s@%s %s%s%s%s%s%s%s%s%s%s\n  │\n  ╰╼╸\ue285 ' \
+      (set_color -b black)(set_color cyan)\ue0b2(set_color normal) \
+      (set_color -b cyan)(set_color black)\uf306 $USER $hostname (set_color normal) \
+      (set_color -b brblack)(set_color black)' '\uf07c' '(prompt_pwd)' '(set_color normal) \
+      (set_color -b black)(set_color brblack)\ue0b0(set_color normal) \
+      (set_color -b white)(set_color black)\ue0b0(set_color normal) \
+      (set_color -b white)(set_color black)' '\ue725(fish_git_prompt)(set_color normal) \
+      (set_color -b black)(set_color white)\ue0b0(set_color normal)
 end
 
 # intro
